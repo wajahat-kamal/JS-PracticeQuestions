3c0be83c5ba624a2ba3f629ff6ad7bfcc08eaaf4
wajahat-kamal <wajahatkamal3.0@gmail.com>
2026-02-12 17:09:07 +0500

    Update reverse_words function to handle dynamic input and return reversed words

--- Beginner_Questions/question_1.js
+++ Beginner_Questions/question_1.js
@@ -1,8 +1,11 @@
 // Reverse Words in a Sentence
 
-let text = "Wajahat Kamal"
+let text = "JavaScript is Fun"
 
 function reverse_words(text) {
+    let split_text = text.split(" ")
+    const reverse_text = split_text.reverse()
 
+    return reverse_text
 }
 console.log(reverse_words(text))
