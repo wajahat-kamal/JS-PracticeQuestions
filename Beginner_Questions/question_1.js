c69fcf00ae374da56f7442b02ec11f3380099d05
wajahat-kamal <wajahatkamal3.0@gmail.com>
2026-02-12 17:14:18 +0500

    Optimize reverse_words function by combining split, reverse, and join operations into a single line for improved readability.

--- Beginner_Questions/question_1.js
+++ Beginner_Questions/question_1.js
@@ -3,10 +3,8 @@
 let text = "JavaScript is Fun"
 
 function reverse_words(text) {
-    const split_text = text.split("")
-    const reverse_text = split_text.reverse("")
-    const join_text = reverse_text.join("")
+    const reverse_text = text.split("").reverse().join("")
 
-    return join_text
+    return reverse_text
 }
 console.log(reverse_words(text))
