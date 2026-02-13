51376de1d17a08b2b99a80506cc82ae729e2a36e
wajahat-kamal <wajahatkamal3.0@gmail.com>
2026-02-13 17:34:41 +0500

    Add password verification and account lock mechanism in login function

--- Beginner_Questions/question_2.js
+++ Beginner_Questions/question_2.js
@@ -3,6 +3,17 @@
 
 function login(password) {
     const realPassword = "admin123"
+    let passwordCount = 0;
+    let accountStatus = "Unlocked"
+
+    if (password !== realPassword) {
+        passwordCount++;
+        return "Incorrect password"
+    }
+    if (passwordCount >= 3) {
+        accountStatus = "Locked"
+        throw new Error("Your account is locked")
+    }
 
 }
 let password = "admin"
