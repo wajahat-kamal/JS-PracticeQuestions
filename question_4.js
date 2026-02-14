dfcec8564a936ce2b96ff0fdf07db1f048dae89d
wajahat-kamal <wajahatkamal3.0@gmail.com>
2026-02-14 07:54:31 +0500

    Remove unused CSS and JavaScript files related to question_3.html, streamlining the project by eliminating unnecessary code and dependencies.

---
+++ question_4.js
@@ -0,0 +1,8 @@
+// Order Processing & Inventory System
+
+const inventory = [
+    { id: 1, name: "Laptop", price: 50000, stock: 5 },
+    { id: 2, name: "Phone", price: 20000, stock: 10 },
+    { id: 3, name: "Headphones", price: 2000, stock: 15 }
+  ];
+  
