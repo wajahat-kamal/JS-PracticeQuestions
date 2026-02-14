c4d7964804302cb3580ef6ba653d479df4d521a9
wajahat-kamal <wajahatkamal3.0@gmail.com>
2026-02-14 09:14:43 +0500

    Enhance createBankAccount function in question_4.js by adding validation for deposit and withdrawal amounts, and implementing a transaction history structure for better account management.

--- question_4.js
+++ question_4.js
@@ -1,25 +1,41 @@
 // Bank Account System
 
-function createBankAccount(ownerName, initialBalance){
+function createBankAccount(ownerName, initialBalance) {
+    let balance = initialBalance
     return {
         deposit: (amount) => {
-           
+            if (amount <= 0) {
+                return "Amount must be more than 0"
+            }
+            balance += amount
+            return balance
+
         },
         withdraw: (amount) => {
-
+            if (amount > balance) {
+                return "Cannot withdraw more than balance"
+            } else if (amount < 0) {
+                return "Cannot withdraw negative"
+            }
+            balance = balance - amount
+            return balance
         },
         getBalance: () => {
-
+            return balance
         },
         transactionHistory: () => {
-
+            return {
+                type: "deposit" | "withdraw",
+                amount: number,
+                balanceAfter: number
+              }
         }
     }
 }
 
 const account = createBankAccount("Wajahat", 10000)
 
-account.deposit(500);
-account.withdraw(200);
-account.getBalance();
-account.transactionHistory();
+console.log(account.deposit(500))
+console.log(account.withdraw(200))
+console.log(account.getBalance())
+// account.transactionHistory()
