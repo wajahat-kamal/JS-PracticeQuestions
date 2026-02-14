fc84a2beab78bff2604d79ce0d5cd3113f3e1c3e
wajahat-kamal <wajahatkamal3.0@gmail.com>
2026-02-14 09:44:39 +0500

    Add createExpenseTracker function in question_5.js to manage expenses, including methods for adding expenses and calculating total expenses.

--- question_5.js
+++ question_5.js
@@ -1,1 +1,29 @@
 // Expense Tracker
+
+
+function createExpenseTracker() {
+    let expense = []
+    return {
+        addExpense: (name, category, amount) => {
+            const newExpense = {
+                name,
+                category,
+                amount
+            }
+            expense.push(newExpense)
+        },
+        getTotalExpenses: () => {
+            const total = expense.reduce((accumulator, currentItem) => {
+                return accumulator + currentItem.amount
+            }, 0)
+            return total
+        }
+    }
+}
+
+const expenseTracker = createExpenseTracker()
+
+expenseTracker.addExpense("Lunch", "Food", 800)
+expenseTracker.addExpense("Lunch", "Food", 800)
+
+console.log(expenseTracker.getTotalExpenses());
