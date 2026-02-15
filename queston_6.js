9994d3d692e509cdd26283452634aab1df9121b6
wajahat-kamal <wajahatkamal3.0@gmail.com>
2026-02-15 09:05:37 +0500

    Add createTaskManager function in queston_6.js to manage tasks, including an addTask method for adding tasks and logging task details.

--- queston_6.js
+++ queston_6.js
@@ -2,5 +2,21 @@
 
 
 function createTaskManager() {
-    
+    let task = {
+        id: number,
+        title: string,
+        completed: false,
+        createdAt: timestamp
+    }
+    return {
+        addTask: (title) => {
+            task.title = title
+            console.log(task);
+
+        }
+    }
 }
+
+const task = createTaskManager()
+
+task.addTask("This is my first task")
