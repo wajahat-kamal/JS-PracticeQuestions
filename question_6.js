a5fcfea554ce9df90dda0d708f690044d6496da8
wajahat-kamal <wajahatkamal3.0@gmail.com>
2026-02-15 09:48:06 +0500

    Add getAllTasks and getCompletedTasks methods to createTaskManager in question_6.js for improved task retrieval. Update example usage to include commented-out calls for these new methods.

--- question_6.js
+++ question_6.js
@@ -31,6 +31,12 @@ function createTaskManager() {
             tasks.splice(index, 1)
             return "Task Deleted Successfully!"
         },
+        getAllTasks: () => {
+            return [...tasks]
+        },
+        getCompletedTasks: () => {
+            return tasks.filter((task) => task.completed === true)
+        }
     }
 }
 
@@ -40,5 +46,9 @@ console.log(task.addTask("This is my first task", 1))
 console.log(task.addTask("This is my second task", 2))
 
 console.log(task.completeTask(2));
-console.log(task.removeTask(2));
+// console.log(task.removeTask(2));
+// console.log(task.getAllTasks());
+// console.log(task.getCompletedTasks());
+
+
 
