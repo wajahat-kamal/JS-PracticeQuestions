9dc080a4331965b214ea83c56d9c52aee8387193
wajahat-kamal <wajahatkamal3.0@gmail.com>
2026-02-15 09:22:03 +0500

    Add completeTask method to createTaskManager in question_6.js for marking tasks as completed and logging task details. Update addTask method to generate unique IDs for tasks.

--- question_6.js
+++ question_6.js
@@ -5,12 +5,23 @@ function createTaskManager() {
     let tasks = []
     return {
         addTask: (title) => {
+            let id = Math.random()
             tasks.push({
-                id: 1,
+                id: id,
                 title,
                 completed: false,
                 createdAt: new Date()
             })
+            console.log("Task Added Successfully!", tasks);
+        },
+        completeTask: (id) => {
+            const task = tasks.find((task) => task.id === id)
+            if (!task) {
+                return "Task not found!"
+            }
+            task.completed = true
+            console.log(tasks);
+            return "Task Completed!"
         },
     }
 }
@@ -18,3 +29,6 @@ function createTaskManager() {
 const task = createTaskManager()
 
 task.addTask("This is my first task")
+task.addTask("This is my second task")
+
+console.log(task.completeTask(0.44794367138786206));
