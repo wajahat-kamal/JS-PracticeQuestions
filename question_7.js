69fdf0d8fc1bd1a1dde1ac4dbe2de4ddf487d712
wajahat-kamal <wajahatkamal3.0@gmail.com>
2026-02-18 09:54:30 +0500

    Refactor vowelsAndConsonants function in question_7.js to log consonants after vowels, improving the output structure and readability.

--- question_7.js
+++ question_7.js
@@ -1,10 +1,15 @@
 
 function vowelsAndConsonants(s) {
+    let constantArr = []
     for (let i = 0; i < s.length; i++) {
         if (s[i] === "a" || s[i] === "e" || s[i] === "i" || s[i] === "o" || s[i] === "u") {
             console.log(s[i])
+        } else{
+            constantArr.push(s[i])
         }
-        console.log(s[i])
+    }
+    for (const n of constantArr) {
+        console.log(n)
     }
 }
 vowelsAndConsonants("JavaScriptLoops")
