9e911a231f617b5c43f0fa7f3be28dfbf230c5a8
wajahat-kamal <wajahatkamal3.0@gmail.com>
2026-02-18 10:11:03 +0500

    Refactor regexVar function in question_8.js to improve vowel detection logic and clean up unnecessary code. Ensure the function checks for strings starting with a vowel and maintains proper structure.

--- question_8.js
+++ question_8.js
@@ -4,15 +4,12 @@ function regexVar(s) {
 
     let vowel = "a" || "e" || "i" || "o" || "u"
 
-    
+
     if (s.startsWith(vowel) && s.endsWith()) {
-        
+
     }
-    
-    
-    /*
-     * Do not remove the return statement
-     */
+
+
     return re;
 }
 console.log(regexVar("abcda"))
