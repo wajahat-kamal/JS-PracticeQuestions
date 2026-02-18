ba2c0bcf344f2c90b52ea03ab2c726d5b9d7e852
wajahat-kamal <wajahatkamal3.0@gmail.com>
2026-02-18 09:44:43 +0500

    Refactor vowelsAndConsonants function in question_7.js to directly log vowels instead of storing them in an array, simplifying the implementation.

--- question_7.js
+++ question_7.js
@@ -1,12 +1,9 @@
 
 function vowelsAndConsonants(s) {
-    let strArray = []
     for (let i = 0; i < s.length; i++) {
         if (s[i] === "a" || s[i] === "e" || s[i] === "i" || s[i] === "o" || s[i] === "u") {
-            strArray.pust(s[i])
+            console.log(s[i])
         }
     }
-    const result = strArray.join('')
-    console.log(result);
 }
 vowelsAndConsonants("JavaScriptLoops")
