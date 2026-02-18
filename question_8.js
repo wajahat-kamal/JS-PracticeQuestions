7fe4f3e33ba8058d3b05a97dc16381b46bb5e396
wajahat-kamal <wajahatkamal3.0@gmail.com>
2026-02-18 10:05:35 +0500

    Add regexVar function in question_8.js to match strings that start and end with the same vowel. Initial implementation includes basic structure and logging for testing.

---
+++ question_8.js
@@ -0,0 +1,18 @@
+
+
+function regexVar(s) {
+
+    let vowel = "a" || "e" || "i" || "o" || "u"
+
+    
+    if (s.startsWith(vowel) && s.endsWith()) {
+        
+    }
+    
+    
+    /*
+     * Do not remove the return statement
+     */
+    return re;
+}
+console.log(regexVar("abcda"))
