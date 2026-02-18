84a907cad4009895a54a8acead57242bec472c56
wajahat-kamal <wajahatkamal3.0@gmail.com>
2026-02-18 09:45:26 +0500

    Log all characters in vowelsAndConsonants function of question_7.js for debugging purposes, in addition to logging vowels.

--- question_7.js
+++ question_7.js
@@ -4,6 +4,7 @@ function vowelsAndConsonants(s) {
         if (s[i] === "a" || s[i] === "e" || s[i] === "i" || s[i] === "o" || s[i] === "u") {
             console.log(s[i])
         }
+        console.log(s[i])
     }
 }
 vowelsAndConsonants("JavaScriptLoops")
