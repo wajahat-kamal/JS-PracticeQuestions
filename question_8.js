1b5c67a06bf07282b81c09c1ecd73c3688f24f98
wajahat-kamal <wajahatkamal3.0@gmail.com>
2026-02-18 10:18:26 +0500

    Refactor regexVar function in question_8.js to simplify vowel checks by consolidating conditions into a single statement, enhancing readability and maintainability.

--- question_8.js
+++ question_8.js
@@ -5,14 +5,14 @@ function regexVar(s) {
     let first = s[0]
     let last = s[s.length - 1]
 
-    if ("aeoui".includes(first)) {
-        if ("aeiou".includes(last)) {
-            if (first === last) {
-                return true
-            } else false;
-        } else false;
-    } else false;
+    const vowel = "aeiou"
+
+    if (vowel.includes(first) && vowel.includes(last) && first === last) {
+        return true;
+    } else {
+        return false;
+    }
 
     // return re;
 }
-console.log(regexVar("bcd"))
+console.log(regexVar("bcda"))
