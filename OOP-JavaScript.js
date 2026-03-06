dbed3afcb22ead8c0f3ad6be70eb404261467a1c
wajahat-kamal <wajahatkamal3.0@gmail.com>
2026-03-06 10:34:04 +0500

    Add user object with details and method in OOP-JavaScript.js to demonstrate object-oriented programming concepts.

--- OOP-JavaScript.js
+++ OOP-JavaScript.js
@@ -1,1 +1,10 @@
-// its not a practice question. learn object oriented programing in js
+// its not a practice question. learn object oriented programing in js.
+
+const user = {
+    username: "Wajahat",
+    age: 16,
+
+    getUserDetail: function (){
+        return `Username is ${this.username} and age is ${this.age}`
+    }
+}
